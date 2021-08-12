111bfdbd4a86466aeb3dbadc030e0e36f5b48b9a
Jesse Hendriks <jesse.hendriks@vodafoneziggo.com>
2021-08-12 10:40:01 +0200

    Included firebase/firestore

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -14,6 +14,7 @@
 import { ref, onMounted } from 'vue';
 import OverviewTable from './OverviewTable';
 import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 export default {
   components: { OverviewTable },
