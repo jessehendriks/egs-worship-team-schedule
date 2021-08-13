e365b62cc55248f329f3c698203568b0ef67b05f
Jesse Hendriks <jesse.hendriks@vodafoneziggo.com>
2021-08-13 08:59:55 +0200

    Update firebase settings

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -13,55 +13,51 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
-
-// messaging.onMessage((payload) => {
-//   console.log('Message received. ', payload);
+// const messaging = firebase.messaging();
+//
+// messaging.getToken({vapidKey: 'BDKujyhV26n9hJQnXKFDP58CC7YvxPJVbYLDqduTqG2RgRLqo60rm4Pvy5-L-Bqqv2wxz8X-lQaDJ8PJNAZUMmI'}).then((currentToken) => {
+//   if (currentToken) {
+//     sendTokenToServer(currentToken);
+//   } else {
+//     console.log('No registration token available. Request permission to generate one.');
+//     setTokenSentToServer(false);
+//   }
+// }).catch((err) => {
+//   console.log('An error occurred while retrieving token. ', err);
+//   setTokenSentToServer(false);
 // });
-
-messaging.getToken({vapidKey: 'BDKujyhV26n9hJQnXKFDP58CC7YvxPJVbYLDqduTqG2RgRLqo60rm4Pvy5-L-Bqqv2wxz8X-lQaDJ8PJNAZUMmI'}).then((currentToken) => {
-  if (currentToken) {
-    sendTokenToServer(currentToken);
-  } else {
-    console.log('No registration token available. Request permission to generate one.');
-    setTokenSentToServer(false);
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  setTokenSentToServer(false);
-});
-
-function sendTokenToServer(currentToken) {
-  if (!isTokenSentToServer()) {
-    console.log('Sending token to server...', currentToken);
-    // TODO(developer): Send the current token to your server.
-    setTokenSentToServer(true);
-  } else {
-    console.log('Token already sent to server so won\'t send it again ' +
-      'unless it changes');
-  }
-}
-
-function isTokenSentToServer() {
-  return window.localStorage.getItem('sentToServer') === '1';
-}
-
-function setTokenSentToServer(sent) {
-  window.localStorage.setItem('sentToServer', sent ? '1' : '0');
-}
-
-function requestPermission() {
-  console.log('Requesting permission...');
-  Notification.requestPermission().then((permission) => {
-    if (permission === 'granted') {
-      console.log('Notification permission granted.');
-      // TODO(developer): Retrieve a registration token for use with FCM.
-      // In many cases once an app has been granted notification permission,
-      // it should update its UI reflecting this.
-    } else {
-      console.log('Unable to get permission to notify.');
-    }
-  });
-}
-
-requestPermission();
+//
+// function sendTokenToServer(currentToken) {
+//   if (!isTokenSentToServer()) {
+//     console.log('Sending token to server...', currentToken);
+//
+//     setTokenSentToServer(true);
+//   } else {
+//     console.log('Token already sent to server so won\'t send it again ' +
+//       'unless it changes');
+//   }
+// }
+//
+// function isTokenSentToServer() {
+//   return window.localStorage.getItem('sentToServer') === '1';
+// }
+//
+// function setTokenSentToServer(sent) {
+//   window.localStorage.setItem('sentToServer', sent ? '1' : '0');
+// }
+//
+// function requestPermission() {
+//   console.log('Requesting permission...');
+//   Notification.requestPermission().then((permission) => {
+//     if (permission === 'granted') {
+//       console.log('Notification permission granted.');
+//       // TODO(developer): Retrieve a registration token for use with FCM.
+//       // In many cases once an app has been granted notification permission,
+//       // it should update its UI reflecting this.
+//     } else {
+//       console.log('Unable to get permission to notify.');
+//     }
+//   });
+// }
+//
+// requestPermission();
